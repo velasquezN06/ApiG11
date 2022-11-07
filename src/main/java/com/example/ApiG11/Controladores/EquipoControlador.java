7d0d7bb65bf797de342c85a2535fa3717364879a
velasquezN06 <114581980+velasquezN06@users.noreply.github.com>
2022-11-07 16:41:38 -0500

    avances
    avances de los microservicios.

---
+++ src/main/java/com/example/ApiG11/Controladores/EquipoControlador.java
@@ -0,0 +1,22 @@
+package com.example.ApiG11.Controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ApiG11.Modelos.EquipoModelo;
+import com.example.ApiG11.Servicios.EquipoServicio;
+
+@RestController
+@RequestMapping("/equipo")
+public class EquipoControlador {
+
+    @Autowired
+    EquipoServicio servicio;
+    @PostMapping("/guardar")
+    public EquipoModelo guardarEquipo(@RequestBody EquipoModelo equipo){
+        return servicio.guardarEquipo(equipo);
+    }
+}
