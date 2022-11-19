5c32841eb3231ac5b7577bc52451e478bc4b570a
velasquezN06 <114581980+velasquezN06@users.noreply.github.com>
2022-11-19 07:23:26 -0500

    nnn

--- src/main/java/com/example/ApiG11/Controladores/UsuarioControlador.java
+++ src/main/java/com/example/ApiG11/Controladores/UsuarioControlador.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.example.ApiG11.Servicios.UsuarioServicio;
 
 @RestController
 @RequestMapping("/usuario")
+@CrossOrigin("*")
 public class UsuarioControlador {
     
     @Autowired
